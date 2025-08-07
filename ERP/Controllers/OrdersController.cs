dfca191e6d0d4185188f9c447170946c02c0255b
david <davidrijo18@gmail.com>
2025-08-07 04:54:23 +0100

    Implemented some endpoints

--- ERP/Controllers/OrdersController.cs
+++ ERP/Controllers/OrdersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ERP.Application.DTOs.Order;
+using ERP.Application.Interfaces;
+using ERP.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ERP.Controllers;
 
@@ -6,5 +9,80 @@ namespace ERP.Controllers;
 [Route("api/[controller]")]
 public class OrdersController : ControllerBase
 {
-    
+    private readonly IOrderService _orderService;
+
+    public OrdersController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    #region GETs
+
+    [HttpGet("{id:Guid}")]
+    public async Task<ActionResult<OrderResponseDTO>> GetById(Guid id)
+    {
+        var order = await _orderService.GetOrderByIdAsync(id);
+
+        if (order is null)
+            return NotFound($"Order with ID '{id}' not found.");
+
+        return Ok(order);
+    }
+
+    #endregion
+
+
+    #region CHANGE ORDER STATUS
+
+    [HttpPatch("cancel/{id:guid}")]
+    public async Task<IActionResult> CancelOrder(Guid id)
+    {
+        await _orderService.CancelOrderAsync(id);
+
+        return Ok();
+    }
+
+    [HttpPatch("submit/{id:guid}")]
+    public async Task<ActionResult<OrderResponseDTO>> SubmitOrder(Guid id)
+    {
+        var order = await _orderService.SubmitOrderAsync(id);
+
+        return Ok(order);
+    }
+
+    [HttpPatch("complete/{id:guid}")]
+    public async Task<ActionResult<OrderResponseDTO>> CompleteOrder(Guid id)
+    {
+        var order = await _orderService.CompleteOrderAsync(id);
+
+        return Ok(order);
+    }
+
+    [HttpPatch("invoice/{id:guid}")]
+    public async Task<ActionResult<OrderResponseDTO>> InvoiceOrder(Guid id)
+    {
+        var order = await _orderService.InvoiceOrderAsync(id);
+
+        return Ok(order);
+    }
+    #endregion
+
+
+    #region ADD, CREATE, REMOVE, UPDATES
+
+    [HttpPost]
+    public async Task<ActionResult<OrderResponseDTO>> CreateOrder([FromBody] CreateOrderDTO orderDTO)
+    {
+        if (orderDTO is null)
+            return BadRequest("Product data must be provided.");
+
+        var order = await _orderService.CreateOrderAsync(orderDTO);
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = order.Id },
+            order
+            );
+    }
+    #endregion
 }
