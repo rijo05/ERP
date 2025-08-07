d298f8dd3a6145119abe698c05c30c2717e7ab59
david <davidrijo18@gmail.com>
2025-08-07 04:55:58 +0100

    small fix

--- ERP/Middleware/ExceptionHandlingMiddleware.cs
+++ ERP/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,54 +1,53 @@
 ﻿using FluentValidation;
 
-namespace ERP.Middleware
+namespace ERP.Middleware;
+
+public class ExceptionHandlingMiddleware
 {
-    public class ExceptionHandlingMiddleware
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
     {
-        private readonly RequestDelegate _next;
-
-        public ExceptionHandlingMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public async Task Invoke(HttpContext httpContext)
-        {
-            try
-            {
-                await _next(httpContext);
-            }
-            catch (ValidationException ex)
-            {
-                httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-
-                httpContext.Response.ContentType = "application/json";
-
-                var erros = ex.Errors.Select(e => new
-                {
-                    field = e.PropertyName,
-                    message = e.ErrorMessage
-                });
-
-                await httpContext.Response.WriteAsJsonAsync(new
-                {
-                    error = "Validation Failed",
-                    errors = erros
-                });
-
-            }
-            catch ( Exception ex)
-            {
-                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-
-                httpContext.Response.ContentType = "application/json";
-
-                await httpContext.Response.WriteAsJsonAsync(new
-                {
-                    error = "Internal Server Error",
-                    errors = ex.Message
-                });
-            }
-        }
-
+        _next = next;
     }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (ValidationException ex)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+            httpContext.Response.ContentType = "application/json";
+
+            var erros = ex.Errors.Select(e => new
+            {
+                field = e.PropertyName,
+                message = e.ErrorMessage
+            });
+
+            await httpContext.Response.WriteAsJsonAsync(new
+            {
+                error = "Validation Failed",
+                errors = erros
+            });
+
+        }
+        catch ( Exception ex)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            httpContext.Response.ContentType = "application/json";
+
+            await httpContext.Response.WriteAsJsonAsync(new
+            {
+                error = "Internal Server Error",
+                errors = ex.Message
+            });
+        }
+    }
+
 }
