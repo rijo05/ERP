01d789f649bc8ffb210e00f47265634e4cc34755
David <a52913@alunos.isel.pt>
2025-08-07 22:06:56 +0100

    created mappers for Order and OrderItem

---
+++ ERP/Application/Mappers/OrderItemMapper.cs
@@ -0,0 +1,46 @@
+﻿using ERP.Application.DTOs.Order;
+using ERP.Application.DTOs.OrderItems;
+using ERP.Application.Services;
+using ERP.Domain.Entities;
+
+namespace ERP.Application.Mappers;
+
+public class OrderItemMapper
+{
+    private readonly HateoasLinkService _hateoasLinkService;
+
+    public OrderItemMapper(HateoasLinkService hateoasLinkService)
+    {
+        _hateoasLinkService = hateoasLinkService;
+    }
+
+    public OrderItemResponseDTO ToOrderItemResponseDTO(OrderItem item)
+    {
+        return new OrderItemResponseDTO
+        {
+            OrderId = item.OrderId,
+            ReferenceId = item.ReferenceId,
+            Name = item.Name,
+            UnitPrice = item.UnitPrice,
+            Quantity = item.Quantity,
+            ItemType = item.ItemType
+        };
+    }
+
+    public List<OrderItemResponseDTO> ToOrderItemResponseDTOList(List<OrderItem> items)
+    {
+        return items.Select(x => ToOrderItemResponseDTO(x)).ToList();
+    }
+
+    //mudar
+    private Dictionary<string, object> GenerateLinks(OrderItem item)
+    {
+        return _hateoasLinkService.GenerateLinksCRUD(
+                    item.ReferenceId,
+                    "OrderItems",
+                    "GetById",
+                    "Update",
+                    "Delete"
+        );
+    }
+}
---
+++ ERP/Application/Mappers/OrderMapper.cs
@@ -0,0 +1,51 @@
+﻿using ERP.Application.DTOs.Order;
+using ERP.Application.DTOs.User;
+using ERP.Application.Services;
+using ERP.Domain.Entities;
+using ERP.Domain.Enums;
+using ERP.Domain.ValueObjects;
+
+namespace ERP.Application.Mappers;
+
+public class OrderMapper
+{
+    private readonly HateoasLinkService _hateoasLinkService;
+    private readonly OrderItemMapper _orderItemMapper;
+
+    public OrderMapper(HateoasLinkService hateoasLinkService, OrderItemMapper orderItemMapper)
+    {
+        _hateoasLinkService = hateoasLinkService;
+        _orderItemMapper = orderItemMapper;
+    }
+
+    public OrderResponseDTO ToOrderResponseDTO(Order order)
+    {
+        return new OrderResponseDTO
+        {
+            Id = order.Id,
+            UserId = order.UserId,
+            CustomerId = order.CustomerId,
+            OrderStatus = order.OrderStatus,
+            CreatedAt = order.CreatedAt,
+            LastUpdatedAt = order.LastUpdatedAt,
+            SubmittedAt = order.SubmittedAt,
+            Items = _orderItemMapper.ToOrderItemResponseDTOList(order.Items.ToList())
+        };
+    }
+
+    public List<OrderResponseDTO> ToOrderResponseDTOList(List<Order> orders)
+    {
+        return orders.Select(x => ToOrderResponseDTO(x)).ToList();
+    }
+
+    private Dictionary<string, object> GenerateLinks(Order order)
+    {
+        return _hateoasLinkService.GenerateLinksCRUD(
+                    order.Id,
+                    "Orders",
+                    "GetById",
+                    "Update",
+                    "Delete"
+        );
+    }
+}
