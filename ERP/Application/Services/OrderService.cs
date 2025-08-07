17d0eeda3b120cd7505fed58cb3ab8f9affa1095
David <a52913@alunos.isel.pt>
2025-08-07 22:13:16 +0100

    small fix

--- ERP/Application/Services/OrderService.cs
+++ ERP/Application/Services/OrderService.cs
@@ -1,19 +1,27 @@
 ﻿using ERP.Application.DTOs.Order;
 using ERP.Application.DTOs.OrderItems;
 using ERP.Application.Interfaces;
+using ERP.Application.Mappers;
 using ERP.Domain.Entities;
 using ERP.Domain.Enums;
 using ERP.Domain.Interfaces;
+using ERP.Persistence.Repository;
 
 namespace ERP.Application.Services;
 
 public class OrderService : IOrderService
 {
     private readonly IOrderRepository _orderRepository;
+    private readonly OrderMapper _orderMapper;
+    private readonly OrderItemMapper _orderItemMapper;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public OrderService(IOrderRepository orderRepository)
+    public OrderService(IOrderRepository orderRepository, OrderMapper orderMapper, OrderItemMapper orderItemMapper, IUnitOfWork unitOfWork)
     {
         _orderRepository = orderRepository;
+        _orderMapper = orderMapper;
+        _orderItemMapper = orderItemMapper;
+        _unitOfWork = unitOfWork;
     }
 
     #region GETs
@@ -28,23 +36,37 @@ public class OrderService : IOrderService
         throw new NotImplementedException();
     }
 
-    public Task<OrderResponseDTO> GetOrderByIdAsync(Guid id)
+    public async Task<OrderResponseDTO> GetOrderByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var order = await _orderRepository.GetByIdAsync(id);
+
+        if (order is null)
+            throw new Exception("Order not found");
+
+        return _orderMapper.ToOrderResponseDTO(order);
     }
 
-    public Task<List<OrderItemResponseDTO>> GetOrderItemsAsync(Guid id)
+    public async Task<List<OrderItemResponseDTO>> GetOrderItemsAsync(Guid id)
     {
-        throw new NotImplementedException();
+        //verificar que o pedido existe
+        if (!await _orderRepository.CheckIfExistsAsync(id))
+            throw new Exception("Order doesnt exist");
+            
+        var orderItems = await _orderRepository.GetOrderItemsAsync(id);
+
+        return _orderItemMapper.ToOrderItemResponseDTOList(orderItems);
     }
 
     #endregion
 
     #region CHANGE ORDER STATUS
 
-    public Task<bool> CancelOrderAsync(Guid id)
+    public async Task CancelOrderAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var order = await EnsureOrderExists(id);
+
+        order.Cancel(); 
+        await _unitOfWork.CommitAsync();
     }
 
     public Task<OrderResponseDTO> CompleteOrderAsync(Guid orderId)
@@ -57,9 +79,13 @@ public class OrderService : IOrderService
         throw new NotImplementedException();
     }
 
-    public Task<OrderResponseDTO> SubmitOrderAsync(Guid id)
+    public async Task<OrderResponseDTO> SubmitOrderAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var order = await EnsureOrderExists(id);
+
+        order.Submit();
+        await _unitOfWork.CommitAsync();
+        return _orderMapper.ToOrderResponseDTO(order);
     }
 
     #endregion
@@ -91,5 +117,13 @@ public class OrderService : IOrderService
         throw new NotImplementedException();
     }
 
+    #endregion
+
+
+    #region private
+    private async Task<Order?> EnsureOrderExists(Guid id)
+    {
+        return await _orderRepository.GetByIdAsync(id) ?? throw new Exception("Order doesn't exist.");
+    }
     #endregion
 }
--- ERP/Domain/Interfaces/IRepository.cs
+++ ERP/Domain/Interfaces/IRepository.cs
@@ -7,4 +7,5 @@ public interface IRepository<T> where T : class
     public Task AddAsync(T entity);
     public Task DeleteAsync(T entity);
     public Task UpdateAsync(T entity);
+    public Task<bool> CheckIfExistsAsync(Guid id);
 }
